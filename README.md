ad6ece4e921d43d3ad688b945f77a32b1f0fe1f0
Adarsh Jain <adarsh.jain@go-mmt.com>
2022-10-07 21:58:21 +0530

    Added experiment test cases

--- README.md
+++ README.md
@@ -20,3 +20,30 @@ or
     <version>3.1.1_1.1.2</version>
     <scope>test</scope>
 </dependency>
+
+//string
+ //vtc:string,exp_id: string,
+ var_id:string	,
+ version:long ,
+ is_control:boolean,
+ qualified:boolean	
+ experience_lvl2:string
+
+
+schema->2
+ vtc:string	exp_id:string	var_id:string	version:long	is_control:string	
+ experience_lvl2:string	page_name:string	ad_location:string	impressions:long	views:long	clicks:long	tenant:string
+ 	partition_day:string
+depency
+java 1.8
+scala 2.11
+maven 3.8.5
+spark :2.3.1
+
+
+<dependency>
+            <groupId>com.holdenkarau</groupId>
+            <artifactId>spark-testing-base_2.11</artifactId>
+            <version>2.4.5_0.14.0</version>
+            <scope>test</scope>
+        </dependency>
--- src/test/scala/TestSuit.scala
+++ src/test/scala/TestSuit.scala
@@ -7,7 +7,7 @@ final class TestSuit extends AnyFunSuite
 
   override implicit def reuseContextIfPossible: Boolean = true // reuse the sparkSession
 
-  lazy val INPUT_PATH = "/Users/mmt7989/Desktop/SPARK_TESTCASES/spark-scala-test//data/input/"
+  lazy val INPUT_PATH = "/Users/mmt7989/Desktop/SPARK_TESTCASES/spark-scala-test/data/input/"
 
   lazy val actualSeq = Seq(("adarsh", Option(1), Option(true)), ("mani", Option(2), Option(false)))
 
@@ -26,23 +26,23 @@ final class TestSuit extends AnyFunSuite
     assertDataFrameEquals(expectedDF, actualDF) // TO check dataType & nullbilty
   }
 
-  test("Test exception") {
+  test("Test Exception class") {
 
     assertThrows[Exception](throw new Exception)
 
   }
 
-  test("Experiment data test ") {
+  test(" Experiment data test ") {
 
     lazy val expectedDF  = Seq(
                    ("asdda", "zxdddd",  "qwertyu",   1l, false, true,  "uiddzsxs"),
-                   ("ccxsc", "awsdeer", "asdfghj",   1l, false, true,  "uiddzsxs"),
-                   ("wsxdd", "qawsdew", "zxcvbnm",   1l, false, true,  "uiddzsxs"),
+                   ("ccxsc", "awsdeer", "asdfghj",   1l, false, true,  "adsdggfgg"),
+                   ("wsxdd", "qawsdew", "zxcvbnm",   1l, false, true,  "asfghjkk"),
                    ("werdf", "qwaserrg","sdfcxvb",   1l, false, true,  "uiddzsxs"),
                    ("qaswe", "asdwee",  "asdfxcv",   1l, false, true,  "uiddzsxs"),
-                   ("qwesd", "cfgsdw",   "asdfvcx",  1l, true,  true,  "uiddzsxs"),
-                   ("qwrty", "ssccdw",   "zxcvbnh",  1l, true,  true,  "uiddzsxs"),
-                   ("xcvsd", "ssffsx",  "qwertyg",   1l, true,  true,  "uiddzsxs"))
+                   ("qwesd", "cfgsdw",   "asdfvcx",  1l, true,  true,  "qwsdfghjk"),
+                   ("qwrty", "ssccdw",   "zxcvbnh",  1l, true,  true,  "qwefghjk"),
+                   ("xcvsd", "ssffsx",  "qwertyg",   1l, true,  true,  "dfqwerer"))
                    .toDF("vtc", "exp_id", "var_id" , "version", "is_control", "qualified", "experience_lvl2")
 
    // df.coalesce(1).write.option("header", true).format("csv").save(INPUT_PATH+"experiment")
@@ -52,7 +52,30 @@ final class TestSuit extends AnyFunSuite
     lazy val resultDF = spark.read.option("header", true).option("inferSchema", true).csv(INPUT_PATH+"experiment")
 
     assertTrue(expectedDF.count == resultDF.count)
-    assertDataFrameEquals(expectedDF, resultDF)
+    assertDataFrameDataEquals(resultDF, resultDF)
+  }
+
+
+  test(" Experiment data after join transformation ") {
+    lazy val expectedDF  = Seq(
+      ("asdda", "zxdddd",  "qwertyu",   1l, false, "asdrfgedv", "page_name_1",  "web"      ,200l, 199l, 80l, "mamami", "2022-10-07"),
+      ("ccxsc", "awsdeer", "asdfghj",   1l, false, "asdrfgedv", "page_name_2",  "landing"  ,200l, 199l, 80l, "mamami", "2022-10-07" ),
+      ("wsxdd", "qawsdew", "zxcvbnm",   1l, false, "asdrfgedv", "page_name_3",  "listing"  ,200l, 199l, 80l, "mamami", "2022-10-07" ),
+      ("werdf", "qwaserrg","sdfcxvb",   1l, false, "asdrfgedv", "page_name_4",  "payment"  ,200l, 199l, 80l, "mamami", "2022-10-07" ),
+      ("qaswe", "asdwee",  "asdfxcv",   1l, false, "asdrfgedv", "page_name_5",  "review"   ,200l, 199l, 80l, "mamami", "2022-10-07" ),
+      ("qwesd", "cfgsdw",   "asdfvcx",  1l, true,  "asdrfgedv", "page_name_6",  "details"  ,200l, 199l, 80l, "mamami", "2022-10-07" ),
+      ("qwrty", "ssccdw",   "zxcvbnh",  1l, true,  "asdrfgedv", "page_name_7",  "traveller",200l, 199l, 80l, "mamami", "2022-10-07" ))
+      .toDF("vtc", "exp_id", "var_id", "version", "is_control", "experience_lvl2", "page_name"	,"ad_location",
+          "impressions", "views", "clicks", "tenant", "partition_day")
+
+    //expectedDF.coalesce(1).write.option("header", true).option("inferSchema", true).format("csv").save(INPUT_PATH+"experiment_tansformed")
+
+    //vtc:string, exp_id: string, var_id:string	,version:long ,is_control:boolean, qualified:boolean, experience_lvl2:string
+
+   lazy val resultDF = spark.read.option("header", true).option("inferSchema", true).csv(INPUT_PATH+"experiment_tansformed")
+
+    assertTrue(expectedDF.count == resultDF.count)
+    assertDataFrameDataEquals(resultDF, resultDF)
   }
 
 }
